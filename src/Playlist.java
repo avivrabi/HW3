37349eadbef93719cff656eae3d0738233e8c26f
Aviv Rabi <aviv.rabi@cmapus.technion.ac.il>
2023-06-16 10:49:48 +0300

    HashCode, equals, toString added to Playlist

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 public class Playlist implements Cloneable, Iterable<Song> {
 
@@ -44,31 +46,80 @@ public class Playlist implements Cloneable, Iterable<Song> {
         StringBuilder sb = new StringBuilder("[");
         SNode currNode = head;
         while ( currNode.nextSong != null ){
+            sb.append("(");
             sb.append(currNode.currSong.toString());
-            sb.append(",");
+            sb.append("),");
             currNode = currNode.nextSong;
         }
 
         // add tail
+        sb.append("(");
         sb.append(currNode.currSong.toString());
+        sb.append(")");
 
         sb.append("]");
         return sb.toString();
     }
 
+    /**
+     * Compares between two playlists to check if equal.
+     * The playlist order doesn't matter and therefore compares two sets.
+     * @param obj Playlist that is a candidate to be equal to
+     * @return boolean represents if the two playlists are equal
+     */
     @Override
-    public boolean equals(Object playlist) {
-        return true;
+    public boolean equals(Object obj) {
+        // if the address is the same
+        if (this == obj) {
+            return true;
+        }
+        // if the playlist is empty or object don't share the same class
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Playlist other = (Playlist) obj;
+        Set<Song> thisSongSet = new HashSet<>();
+        SNode currentThis = head;
+        while (currentThis != null) {
+            thisSongSet.add(currentThis.currSong);
+            currentThis = currentThis.nextSong;
+        }
+        Set<Song> otherSongSet = new HashSet<>();
+        SNode currentOther = other.head;
+        while (currentOther != null) {
+            otherSongSet.add(currentOther.currSong);
+            currentOther = currentOther.nextSong;
+        }
+        return thisSongSet.equals(otherSongSet);
     }
 
+    /**
+     * Calculates a hash code for a given playlist
+     * @return playlist's hashcode
+     */
     @Override
     public int hashCode() {
-        return 0;
-
+        int hashCode = 0;
+        for(Song song: this) {
+            hashCode = 31 * hashCode + song.hashCode();
+        }
+        return hashCode;
     }
 
     @Override
-    public Iterator iterator() {
+    public Playlist clone() {
+        try {
+            Playlist clone = (Playlist) super.clone();
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+
+    @Override
+    public Iterator<Song> iterator() {
         return new PlaylistIterator();
     }
 
@@ -84,8 +135,7 @@ public class Playlist implements Cloneable, Iterable<Song> {
         public Song next() {
             pointer = currNode;
             currNode = currNode.nextSong;
-            Song next = new Song(currNode.currSong);
-            return next;
+            return new Song(currNode.currSong);
         }
     }
 }
